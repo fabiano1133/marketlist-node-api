a7f47a6f73fe6e0962d2e842465162654c3ee53d
Fabiano Albuquerque Gonçalves dos Santos <fabianoalbuquerquegoncalvesdossantos@Mac-mini-de-Fabiano.local>
2023-11-20 13:23:55 -0300

    chore(marketlist-node-api): alteracao arquivos de dependencias

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,6 @@ import { env } from "./env";
 app
   .listen({
     port: env.API_PORT,
+    host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
   })
   .then(() => console.log(`Apliccation is Running`));
